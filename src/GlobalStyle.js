37c417a03cfbb228b04dac0f52e65f1380d68043
MarioT <dev@mario.cx>
2020-11-03 19:52:47 +0100

    fix styles

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -14,10 +14,14 @@ export default createGlobalStyle`
         font-size: 112.5%;
         background-image: url(${help});
         margin: 0;
-        font-family: sans-serif;
+        font-family: Calibri;
         background-size: cover;
         background-repeat: no-repeat;
         background-attachment: fixed;
+    }
+
+    button:focus, textarea:focus, input:focus{
+        outline: none;
     }
 
 `
